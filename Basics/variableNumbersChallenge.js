bb2cc9548f0a3c9210edf69d8af54557cd3afa96
Memnoc <smartdroidesign@gmail.com>
2020-06-06 10:23:09 +0100

    new: numbers and numbers challenge

---
+++ Basics/variableNumbersChallenge.js
@@ -0,0 +1,65 @@
+/**
+ * Challenge #1
+ * Create a method to convert fahrenheit to celsius
+ * The conversion formula is: (fahrenheit - 32) * (5/9)
+ */
+
+const fahrenheitToCelsius = temperature => {
+    let fahrenheit = temperature;
+    let conversion = (temperature - 32) * (5 / 9);
+    return console.log(`${fahrenheit} fahrenheit is ${conversion} celsius`);
+}
+
+fahrenheitToCelsius(45);
+
+/**
+ * Challenge #2
+ * Create a method to convert celsius to fahrenheit 
+ * The conversion formula is: celsius * 9/5 + 32
+ */
+
+const celsiusToFahrenheit = temperature => {
+    let celsius = temperature;
+    let conversion = temperature * 9 / 5 + 32;
+    return console.log(`${celsius} celsius is ${conversion} fahrenheit`);
+}
+
+celsiusToFahrenheit(60);
+
+/**
+ * Challenge #3
+ * Create a method to convert miles to kilometers 
+ * 1 mile = 1.609344 km
+ */
+
+const milesToKilometers = mile => {
+    let miles = mile;
+    let conversion = mile * 1.6;
+    return console.log(`${miles} miles is ${conversion} kilometers`);
+}
+
+milesToKilometers(60);
+
+/**
+ * Challenge #4
+ * Create a method to convert kilometers to miles 
+ * 1 km = 0.621371 miles
+ */
+
+const kilometersToMiles = kilometers => {
+    let kilometer = kilometers;
+    let conversion = kilometers / 1.6;
+    return console.log(`${kilometers} kilometers is ${conversion} miles`);
+}
+
+kilometersToMiles(60);
+
+/**
+ * OUTPUT
+******************************************
+45 fahrenheit is 7.222222222222222 celsius
+60 celsius is 140 fahrenheit
+60 miles is 96 kilometers
+60 kilometers is 37.5 miles
+*******************************************
+ */
---
+++ Basics/variablenumbers.js
@@ -0,0 +1,23 @@
+// create numeric variable
+let num1 = 10;
+
+console.log(`The number in the num1 variable is ${num1}`);
+/*
++ is the addition operator
+- is the subtraction operator
+* is the multiplication operator
+/ is the division operator
+% is the reminder (modulus) operator
+*/
+
+/**
+ * Mini challenge: declare a new num variable.
+ * Come up with an output that uses all the operators.
+ */
+let num2 = 8;
+
+console.log(`${num1} plus ${num2} = ${num1 + num2}`);
+console.log(`${num1} minus ${num2} = ${num1 - num2}`);
+console.log(`${num1} times ${num2} = ${num1 * num2}`);
+console.log(`${num1} divided ${num2} = ${num1 / num2}`);
+console.log(`${num1} reminder ${num2} = ${num1 % num2}`);
