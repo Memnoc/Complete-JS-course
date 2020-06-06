a342d625d967acdc89c2fd1fe45a1daba8a9995b
Memnoc <smartdroidesign@gmail.com>
2020-06-06 09:28:52 +0100

    new: variables and challenge

---
+++ Basics/variablesChallenge.js
@@ -0,0 +1,10 @@
+/**
+ * The output must be 'congratulations {firstName} + {secondName}, you have solved your first JavaScript challenge'.
+ */
+
+let firstName, secondName, message;
+firstName = 'Memnoc';
+secondName = 'Star';
+message = 'you have solved your first JavaScript challenge!';
+
+console.log(`Congratulations ${firstName} ${secondName}, ${message}`);
---
+++ Basics/variablestrings.js
@@ -0,0 +1,11 @@
+// Defining a bunch of variables
+let myGreetings = 'Hello JavaScript';
+
+// Variable to hod first name
+let firstName;
+firstName = 'Memnoc';
+
+// Create the output
+console.log('Hello ' + firstName + '! ' + myGreetings);
+
+// Concatenation
